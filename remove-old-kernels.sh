db53dec4e40480689db9fc4979a4cbf37ef466be
Jason Burns <burns@bbbburns.com>
2017-09-27 14:51:41 -0400

    Updated remove-old-kernels.sh to find only kernels
    Previously the regex linux-* was a little bit too broad. We should narrow this down to linux-image* just to be a little safer and not remove any non-kernel packages like linux-libc-dev!

--- remove-old-kernels.sh
+++ remove-old-kernels.sh
@@ -2,14 +2,14 @@
 # Scrounged up via Google and cobbled together with some basic shell script
 # print the old kernels and remove them if acceptable
 
-dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | xargs sudo apt-get --dry-run remove
+dpkg -l linux-image* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | xargs sudo apt-get --dry-run remove
 
 echo "Really remove these? (yes / no)"
 read DELETEBOOLEAN
 
 if [ $DELETEBOOLEAN = "yes" ]; then
   echo "OK, I'll do it!"
-  dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | xargs sudo apt-get -y remove
+  dpkg -l linux-image* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | xargs sudo apt-get -y remove
 else
   echo "Fine, I won't."
 fi
